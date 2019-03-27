698e8d015ee829179f1996f4d15913b1d66f3aa6
Abdullah Joud <joud@kfupm.edu.sa>
2019-03-27 07:06:24 +0300

    added two fields in the filterset for InsuranceDebt, id and sub_contract

--- debtcollect/debtcollect/insurancedebt/filters.py
+++ debtcollect/debtcollect/insurancedebt/filters.py
@@ -1,20 +1,17 @@
 import django_filters as filters
-
-from dal import autocomplete
-from django.db.models import Q
-from django import forms
-from django.contrib.auth.models import User as MyUser
 from django.utils.translation import ugettext_lazy as _
-
 from .models import *
 
 
 class InsuranceDebtFilter(filters.FilterSet):
+
     class Meta:
         model = InsuranceDebt
         fields = {
+            'sub_contract': ['exact'],
             'type': ['exact'],
             'status': ['exact'],
+            'id': ['exact'],
             'driver_government_id': ['icontains'],
             'driver_mobile': ['icontains'],
         }
@@ -22,6 +19,6 @@ class InsuranceDebtFilter(filters.FilterSet):
     # def custom_title_filter(self, queryset, name, value):
     #     return queryset.filter(Q(title_ar__icontains=value) | Q(title_en__icontains=value))
 
-    # def __init__(self, *args, **kwargs):
-    #     super(ProjectFilter, self).__init__(*args, **kwargs)
-    #     self.filters['client'].widget = autocomplete.ModelSelect2(url='client-autocomplete', )
+    def __init__(self, *args, **kwargs):
+        super(InsuranceDebtFilter, self).__init__(*args, **kwargs)
+        self.filters['id'].label = _('Insurance Debt No')
