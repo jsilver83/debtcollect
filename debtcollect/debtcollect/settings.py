3e04817c73f80abd3f49d58306016b54262af9a5
Abdullah Joud <joud@kfupm.edu.sa>
2019-08-13 11:13:20 +0300

    increased DATA_UPLOAD_MAX_NUMBER_FIELDS in the settings

--- debtcollect/debtcollect/settings.py
+++ debtcollect/debtcollect/settings.py
@@ -183,6 +183,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
+
 # Override settings using environment-specific settings, if any
 try:
     from .local_settings import *
